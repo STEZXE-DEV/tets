49b1f36bc1f96226e56678aec9b0721b2e881726
ST9-DEV <stanislawjurek.ina@gmail.com>
2023-10-27 14:48:16 +0200

    Aktualizacja rekordu.

---
+++ connect_to_database/connect10.php
@@ -0,0 +1,35 @@
+<?php
+$servername ="localhost";
+$username = "root";
+$password ="";
+$database="baza2";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+
+if (!$conn) {
+    die("Connection failed." . mysqli_connect_error());
+
+
+}
+echo "Connected successfully";
+////$sql = "CREATE TABLE guests(
+//    id INT(6),
+//    imie varchar(30) NOT NULL,
+//    nazwisko varchar(30) NOT NULL)"; 
+$sql = "UPDATE guests SET nazwisko='Doe' WHERE id='2'" ;
+$result = mysqli_query($conn, $sql);
+
+echo '<br>';
+
+if (mysqli_query($conn, $sql)){
+    
+    echo "Record updated";
+        
+    }
+   
+          
+    else {
+        echo "Updated 0 records";
+    }
+?>
