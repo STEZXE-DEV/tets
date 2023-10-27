c38efcdb6cd0b301d899cd91995038356027cf64
ST9-DEV <stanislawjurek.ina@gmail.com>
2023-10-27 15:33:56 +0200

    Wysyłanie pobranych danych z formularza do bazy danych.

--- forms/form_use/form_post.php
+++ forms/form_use/form_post.php
@@ -3,4 +3,27 @@ Welcome
 echo $_POST['name'];
 ?>
 <br>
-Your email address is: <?php echo $_POST['email']; ?>
+Your email address is: <?php echo $_POST['email']."<br>"; ?>
+
+<?php
+
+$nazwa_serwera = 'localhost';
+$nazwa_uzyt = 'root';
+$haslo = '';
+$baza="baza2";
+$conn = mysqli_connect($nazwa_serwera, $nazwa_uzyt, $haslo, $baza);
+if (!$conn){
+    die('Połączenie nieudane '. mysqli_connect_error());
+}
+echo "Połączono się pomyślnie<br>";
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$sql="INSERT INTO guests (imie, email) VALUES ('$name', '$email')";
+echo $sql. "<br>";
+if (mysqli_query($conn, $sql)){
+    echo "Dodano rekord do bazy.";
+}
+else {
+    echo "ERROR". $sql."<br>". mysqli_error($conn);
+}
